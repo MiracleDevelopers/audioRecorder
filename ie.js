724b4a82ebe6a8c0aa0fdeb0fb9270106baa588c
Termination <747985842@qq.com>
2017-12-05 18:20:10 +0800

    audio

---
+++ ie.js
@@ -0,0 +1,31 @@
+window.onload=function () {
+    var mb = myBrowser();
+    var myModal=document.getElementById('myModal');
+    var close=document.getElementById('close');
+    if ("IE" == mb) {
+        myModal.style.display='block';
+
+    }
+    close.onclick=function () {
+        myModal.style.display='none';
+    }
+}
+function myBrowser(){
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isOpera = userAgent.indexOf("Opera") > -1;
+    if (isOpera) {
+        return "Opera"
+    }; //判断是否Opera浏览器
+    if (userAgent.indexOf("Firefox") > -1) {
+        return "FF";
+    } //判断是否Firefox浏览器
+    if (userAgent.indexOf("Chrome") > -1){
+        return "Chrome";
+    }
+    if (userAgent.indexOf("Safari") > -1) {
+        return "Safari";
+    } //判断是否Safari浏览器
+    if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+        return "IE";
+    }; //判断是否IE浏览器
+}
